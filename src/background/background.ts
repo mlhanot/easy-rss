2948e06dbdffb7ff383996ed64d46a48fbfb5ffa
Marien-Lorenzo Hanot <68719722+mlhanot@users.noreply.github.com>
2026-04-20 11:43:11 +0100

    Fixed badge not updating

--- src/background/background.ts
+++ src/background/background.ts
@@ -3,6 +3,21 @@ import { fetchLength } from "./length";
 import { updateData } from "./updateData";
 import { getFeeds } from "./feedsInterface";
 
+async function updateBadge(read: string[], entries: Entry[], missingPerms: string[]) {
+  if (missingPerms.length > 0) {
+    browser.browserAction.setBadgeBackgroundColor({ color: "#dddd0d" });
+    browser.browserAction.setBadgeText({ text: "⚠" });
+  } else {
+    let unread = 0;
+    for (const entry of entries) if (read.indexOf(entry.id) === -1) unread++;
+    const hideText : boolean = (await browser.storage.sync.get({hideBadgeText: false})).hideBadgeText;
+    browser.browserAction.setBadgeBackgroundColor({ color: "#dd2e44" });
+    browser.browserAction.setBadgeText({
+      text: (unread === 0 || hideText) ? "" : unread.toString()
+    });
+  }
+}
+
 const sorter = (a: Entry, b: Entry) =>
 	new Date(b.date).getTime() - new Date(a.date).getTime();
 
@@ -52,17 +67,7 @@ async function fetchFeeds() {
   newEntries.forEach((nE) => (entries.some((cE) => nE.id === cE.id) ? null : entries.push(nE)));
 	entries.sort(sorter);
 
-  if (entries.length == 0) { // onChanged is not fired when nothing is pushed
-    if (missingPerms.length == 0) {
-      browser.browserAction.setBadgeBackgroundColor({ color: "#dd2e44" });
-      browser.browserAction.setBadgeText({ text: "" });
-    } else {
-      browser.browserAction.setBadgeBackgroundColor({ color: "#dddd0d" });
-      browser.browserAction.setBadgeText({ text: "⚠" });
-    }
-	  browser.storage.local.set({ missingPerms: missingPerms as unknown as StorageValue });
-    return;
-  }
+  browser.storage.local.get({ read: [] }).then((results)=>updateBadge(results.read,entries,missingPerms));
 	browser.storage.local.set({ entries: entries as unknown as StorageValue, missingPerms: missingPerms as unknown as StorageValue });
 
   // Fetch length after setting the other entry as it can request a lot of pages
@@ -108,30 +113,15 @@ updateData().then(()=>{
       });
     }
 
-    if ((changes.read || changes.entries || changes.missingPerms) && areaName === "local") {
+    if ((changes.read || changes.missingPerms) && areaName === "local") {
       const read: string[] = changes.read && changes.read.newValue
         ? changes.read.newValue
         : (await browser.storage.local.get({ read: [] })).read;
-      const entries: Entry[] = changes.entries && changes.entries.newValue
-        ? changes.entries.newValue
-        : (await browser.storage.local.get({ entries: [] })).entries;
-      const missingPerms: Entry[] = changes.missingPerms && changes.missingPerms.newValue
+      const entries: Entry[] = (await browser.storage.local.get({ entries: [] })).entries;
+      const missingPerms: string[] = changes.missingPerms && changes.missingPerms.newValue
         ? changes.missingPerms.newValue
         : (await browser.storage.local.get({ missingPerms: [] })).missingPerms;
-
-      if (missingPerms.length > 0) {
-        browser.browserAction.setBadgeBackgroundColor({ color: "#dddd0d" });
-        browser.browserAction.setBadgeText({ text: "⚠" });
-      } else {
-        let unread = 0;
-        for (const entry of entries) if (read.indexOf(entry.id) === -1) unread++;
-
-        const hideText : boolean = (await browser.storage.sync.get({hideBadgeText: false})).hideBadgeText;
-        browser.browserAction.setBadgeBackgroundColor({ color: "#dd2e44" });
-        browser.browserAction.setBadgeText({
-          text: (unread === 0 || hideText) ? "" : unread.toString()
-        });
-      }
+      updateBadge(read,entries,missingPerms);
     }
   });
 });
